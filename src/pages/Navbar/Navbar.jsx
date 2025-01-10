006692686981b48e7de2b3287dafd23b681dc14b
jinhyo <kjhyo0220@gmail.com>
2025-01-10 15:38:52 +0900

    Design: 배경 어두워지게 #2

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -13,35 +13,42 @@ const Navbar = () => {
 
   return (
     <div>
-      <S.TopContainer>
-        <S.FishIcon onClick={toggleSidebar}>
-          <img src={navIcon}></img>
-        </S.FishIcon>
-        <img src={navLogo}></img>
-      </S.TopContainer>
-      <S.SidebarContainer isOpen={isSidebarOpen}>
-        <S.SidebarContent isOpen={isSidebarOpen}>
+      {isSidebarOpen && <S.Overlay />} {/* 배경 어둡게 */}
+      <S.NavContainer>
+        {/* 상단바 - 아이콘이랑 로고 이미지 */}
+        <S.TopContainer>
           <S.FishIcon onClick={toggleSidebar}>
-            닫기<img src={navIcon}></img>
+            <img src={navIcon}></img>
           </S.FishIcon>
+          <img src={navLogo}></img>
+        </S.TopContainer>
 
-          <Link to={"/todo"} style={{ fontSize: "24px" }}>
-            하루 기록
-          </Link>
-          <br />
-          <Link to={"/daily"} style={{ fontSize: "24px" }}>
-            하루 비율
-          </Link>
-          <br />
-          <Link to={"/monthly"} style={{ fontSize: "24px" }}>
-            한달 비율
-          </Link>
-          <br />
-          <Link to={"/mypage"} style={{ fontSize: "24px" }}>
-            마이페이지
-          </Link>
-        </S.SidebarContent>
-      </S.SidebarContainer>
+        {/* navbar(sidebar) */}
+        <S.SidebarContainer isOpen={isSidebarOpen}>
+          <S.SidebarContent isOpen={isSidebarOpen}>
+            <S.FishIcon onClick={toggleSidebar}>
+              <img src={navIcon}></img>
+            </S.FishIcon>
+
+            {/* 메뉴 */}
+            <Link to={"/todo"} style={{ fontSize: "24px" }}>
+              하루 기록
+            </Link>
+            <br />
+            <Link to={"/daily"} style={{ fontSize: "24px" }}>
+              하루 비율
+            </Link>
+            <br />
+            <Link to={"/monthly"} style={{ fontSize: "24px" }}>
+              한달 비율
+            </Link>
+            <br />
+            <Link to={"/mypage"} style={{ fontSize: "24px" }}>
+              마이페이지
+            </Link>
+          </S.SidebarContent>
+        </S.SidebarContainer>
+      </S.NavContainer>
     </div>
   );
 };
--- src/pages/Navbar/Navbar.style.js
+++ src/pages/Navbar/Navbar.style.js
@@ -1,15 +1,35 @@
 import styled from "styled-components";
 
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 999; //Sidebar 위에 나타나도록
+`;
+
+export const NavContainer = styled.div`
+  position: relative;
+  z-index: 1000; // Overlay 아래로
+`;
+
 export const SidebarContainer = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   height: 100%;
-  width: ${(props) => (props.isOpen ? "71vw" : "0")};
-  background-color: #f4f4f4;
+  width: ${(props) => (props.isOpen ? "min(71vw,280px)" : "0")};
+
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 0px 50px 50px 0px;
+
   overflow-x: hidden;
   transition: width 0.3s ease;
-  box-shadow: ${(props) => (props.isOpen ? "2px 0 5px rgba(0,0,0,0.3)" : "none")};
+
+  z-index: 1000; // Overlay 위로
 `;
 
 export const SidebarContent = styled.nav`
