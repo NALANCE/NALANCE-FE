4aaab85c9a2ad4cb5c3aaaa77a8f75b20f3f4f88
juyeonnnn <opff5423@gmail.com>
2025-01-16 16:48:05 +0900

    Feat: 문의하기 페이지 제작 #18

--- src/pages/Mypage/Ask/Ask.jsx
+++ src/pages/Mypage/Ask/Ask.jsx
@@ -1,9 +1,18 @@
-import Topbar from "components/Topbar/Topbar";
+import Topbar from 'components/Topbar/Topbar';
+import BottomLogo from 'components/BottomLogo/BottomLogo';
+import cat from 'assets/icons/ask_cat.svg';
+import speech from 'assets/icons/ask_speech.svg';
+import * as S from './Ask.style';
 
 const Ask = () => {
   return (
     <>
       <Topbar pageTitle="문의하기" />
+      <S.Container>
+        <S.Cat src={cat} alt="고양이" />
+        <S.Speech src={speech} alt="말풍선" />
+      </S.Container>
+      <BottomLogo />
     </>
   );
 };
---
+++ src/pages/Mypage/Ask/Ask.style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 246px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+`;
+
+export const Cat = styled.img`
+  width: 80px;
+  height: 73.125px;
+  flex-shrink: 0;
+`;
+
+export const Speech = styled.img`
+  width: 269px;
+  height: 122.951px;
+`;
