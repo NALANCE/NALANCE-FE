dab1ee993028fb0438dbaf71f91ecb90e54b63c2
juyeonnnn <opff5423@gmail.com>
2025-02-04 16:09:23 +0900

    Feat: api 연동 #4

--- src/components/DropBtn/DropBtn.jsx
+++ src/components/DropBtn/DropBtn.jsx
@@ -1,25 +1,23 @@
 import React, { useState } from 'react';
 import * as S from './DropBtn.style';
-import { useNavigate } from 'react-router-dom';
 import btn1 from 'assets/icons/drop_btn1.svg';
 import btn2 from 'assets/icons/drop_btn2.svg';
 
-const DropBtn = ({ link }) => {
-  const navigate = useNavigate();
+const DropBtn = ({ onClick }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isClicked, setIsClicked] = useState(false);
 
-  const onClick = () => {
+  const handleClick = () => {
     setIsClicked(true);
     setTimeout(() => {
-      navigate(link);
+      onClick && onClick();
       setIsClicked(false);
     }, 100);
   };
 
   return (
     <S.DBtn
-      onClick={onClick}
+      onClick={handleClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
