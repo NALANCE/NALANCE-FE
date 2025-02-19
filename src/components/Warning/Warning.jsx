595015c2460429946b2574a40830a3d59b6aa66a
jinhyo <kjhyo0220@gmail.com>
2025-02-19 16:00:28 +0900

    Fix: 파일 이름 대문자 -> 소문자 #98

--- src/components/Warning/Warning.jsx
+++ src/components/Warning/Warning.jsx
@@ -1,5 +1,6 @@
 import sadCat from "assets/img/sadCatBig.png";
 import defaultCat from "assets/img/defaultCatBig.png";
+
 import Notification from "components/common/Notification/Notification";
 import * as S from "./Warning.style";
 import { useEffect, useState } from "react";
